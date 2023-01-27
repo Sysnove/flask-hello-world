608faacba1bbcff0e73c03d67494257437fa49a3
vstung <vstungz@gmail.com>
2023-01-27 17:49:37 +0700

    add env vars show

--- hello.py
+++ hello.py
@@ -1,12 +1,22 @@
 # Shamelessly copied from http://flask.pocoo.org/docs/quickstart/
 
 from flask import Flask
+import os
+import json
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/env')
+def env_show():
+    result = []
+    for k, v in os.environ.items():
+        result.append({k: v})
+    return json.dumps(result)
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
